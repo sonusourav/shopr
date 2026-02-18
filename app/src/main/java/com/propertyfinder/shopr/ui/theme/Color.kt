b7e1ca31eb0ca48a347c80cac5c1e76d6c073eb5
SSV <sonu.sourav@rapido.bike>
2026-02-18 08:55:56 +0530

    chore: setup theme and colors

---
+++ app/src/main/java/com/propertyfinder/shopr/ui/theme/Color.kt
@@ -0,0 +1,37 @@
+package com.propertyfinder.shopr.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Grocery list app palette (matching design)
+val GradientStart = Color(0xFF7C4DFF)
+val GradientEnd = Color(0xFF448AFF)
+val AddCardBackground = Color(0xFFE8E0F5)
+val MilkBlue = Color(0xFF2196F3)
+val VegetablesGreen = Color(0xFF81C784)
+val FruitsRed = Color(0xFFE57373)
+val BreadsAmber = Color(0xFFFFD54F)
+val MeatsPink = Color(0xFFF8BBD9)
+val EmptyStateGray = Color(0xFF9E9E9E)
+val ButtonDark = Color(0xFF424242)
+val CategoryChipUnselectedBg = Color(0xFFF5F5F5)
+val CategoryChipUnselectedText = Color(0xFF757575)
+
+// Category chip: pastel backgrounds (unselected) and text colors
+val MilkChipBg = Color(0xFFE3F2FD)
+val VegetablesChipBg = Color(0xFFE8F5E9)
+val FruitsChipBg = Color(0xFFFFEBEE)
+val BreadsChipBg = Color(0xFFFFF8E1)
+val MeatsChipBg = Color(0xFFFCE4EC)
+val VegetablesChipText = Color(0xFF2E7D32)
+val FruitsChipText = Color(0xFFC62828)
+val BreadsChipText = Color(0xFFF57F17)
+val MeatsChipText = Color(0xFFAD1457)
+val SelectedChipBlue = Color(0xFF2196F3)
